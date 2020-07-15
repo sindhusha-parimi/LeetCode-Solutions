43cc7d256dcdb4f95339bc69fd82a110af5c7995
Sindhusha Devi Parimi <55471005+sindhusha-parimi@users.noreply.github.com>
2020-07-15 18:54:56 -0500

    Create BinaryTreeLevelOrderTraversal2.java

---
+++ July 2020 LC Challenge/BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+ class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+        List<List<Integer>> res=new ArrayList<List<Integer>>();
+        if(root == null) 
+            return new ArrayList<List<Integer>>(); 
+        Queue<TreeNode> q=new LinkedList<TreeNode>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int n=q.size();
+            List<Integer> l=new ArrayList<Integer>();
+            for(int i=0;i<n;i++){
+                TreeNode temp=q.poll();
+                l.add(temp.val);
+             if(temp.left!=null) q.add(temp.left);
+            if(temp.right!=null) q.add(temp.right);
+            }
+            res.add(l);
+             
+           
+        }Collections.reverse(res);
+        return res; 
+        } 
+        
+        
+        
+    }
