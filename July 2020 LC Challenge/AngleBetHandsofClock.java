ef63643baa1398a6b366e42e42c8f7550af2c53c
Sindhusha Devi Parimi <55471005+sindhusha-parimi@users.noreply.github.com>
2020-07-21 18:01:18 -0500

    Day 14, #1344

---
+++ July 2020 LC Challenge/AngleBetHandsofClock.java
@@ -0,0 +1,7 @@
+public double angleClock(int hour, int minutes) {
+        
+       
+         double angle=Math.abs((hour*60+minutes)*0.5-minutes*6);
+        return angle>180.0?360.0-angle:angle;
+        
+    }
